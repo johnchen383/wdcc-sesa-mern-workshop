96f95d601d76c65cd2436e068f9f7b3bc088e65c
Serena Lau (slau981) <serena.lau272@gmail.com>
2021-07-31 13:23:44 +1200

    set up main route functions

--- backend/src/routes/api/index.js
+++ backend/src/routes/api/index.js
@@ -6,10 +6,18 @@ router.get('/', function(req, res) {
     res.send("Hello John!");
 });
 
+// Get cat status 
 router.get('/status', function(req, res) {
     res.send("Hello yozazazaz!");
 });
 
+// Change cat status 
 router.put('/status', () => {})
+
+// Fetch from database
+router.get('/database', () => {})
+
+// Add to database 
+router.put('/database', () => {})
 
 module.exports = router;
