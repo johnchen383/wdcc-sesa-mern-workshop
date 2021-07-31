b621478da31f8f7148a8d97c07aaf00e69c46917
John Chen <john.bm.chen@gmail.com>
2021-07-31 20:46:31 +1200

    yay we're done for day one oiadhasdijdjasjdoasidjoadjiosdjaijdoiasjd

--- frontend/src/pages/IndexPage.js
+++ frontend/src/pages/IndexPage.js
@@ -1,24 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styling/index.css";
 import "../styling/w3.css";
 import Button from "../components/Button";
 import exampleCall from "../api/exampleCall";
+import axios from "axios";
+
 //import CatGif from '../components/cat.js';
 
-function showAlert() {
-  alert("You just clicked the button!");
-}
+// function showAlert() {
+//   alert("You just clicked the button!");
+// }
 
-async function callServer() {
-  const response = await exampleCall();
-  if (response.success) {
-    alert(`Server says: ${JSON.stringify(response.data)}`);
-  } else {
-    alert(`Server had an error: ${JSON.stringify(response.error)}`);
-  }
-}
+// async function callServer() {
+//   const response = await exampleCall();
+//   if (response.success) {
+//     alert(`Server says: ${JSON.stringify(response.data)}`);
+//   } else {
+//     alert(`Server had an error: ${JSON.stringify(response.error)}`);
+//   }
+// }
 
 function IndexPage() {
+  const [keyword, setKeyword] = useState(
+    "asasdfhudsfhsufhusdhfudhfusdhfuojsifd"
+  );
+  const [content, setContent] = useState("");
+  const [myType, setType] = useState("joke");
+
+  async function addContent() {
+    axios
+      .post("http://localhost:3001/api/database", {
+        id: keyword,
+        content: content,
+        type: myType,
+      })
+      .then((res) => {
+        console.log(res);
+      });
+  }
+
+  function handleKeyChange(event) {
+    setKeyword(event.target.value);
+  }
+
+  // function handleTypeChange(event) {
+  //   var thingToReturn = document.querySelector('#typeSelectorKids').value;
+  //   setType(thingToReturn);
+  // }
+
+  function handleTypeChange(event) {
+    setType(event.target.value);
+  }
+
+  function handleContentChange(event) {
+    setContent(event.target.value);
+  }
+
   return (
     <div id={"index-container"}>
       <div className={"spacer spacerTop"} />
@@ -31,7 +68,7 @@ function IndexPage() {
         <input
           className={"search_input"}
           type="search"
-          placeholder="Search"
+          placeholder="Oodle"
           id="site-search"
           name="q"
           aria-label="Search through data"
@@ -43,26 +80,44 @@ function IndexPage() {
           src="https://media.tenor.com/images/dab9fdca9a4f9125b5fae019ec84550c/tenor.gif"
         />
         <div className={"progress-scaf"}>
-          <div class={"progress"}>_</div>
+          <div className={"progress"}>_</div>
         </div>
-        <form class="noodle_form">
-          <div class="cust_grid">
+        <form className={"noodle_form"}>
+          <div className={"cust_grid"}>
             <label for="key">Keyword:</label>
-            <input type="text" id="key" placeholder="Enter keyword" />
+            <input
+              type="text"
+              id="key"
+              placeholder="Enter keyword"
+              onChange={handleKeyChange}
+            />
             <label for="type">Type:</label>
-            <select id="country" name="country">
-              <option value="joke">Joke</option>
-              <option value="fact">Fact</option>
-              <option value="video">Video</option>
-              <option value="gif">Gif</option>
+            <select id="typeSelectorKids" onChange={handleTypeChange} name="type">
+              <option value="joke" onChange={handleTypeChange}>
+                Joke
+              </option>
+              <option value="fact" onChange={handleTypeChange}>
+                Fact
+              </option>
+              <option value="video" onChange={handleTypeChange}>
+                Video
+              </option>
+              <option value="gif" onChange={handleTypeChange}>
+                Gif
+              </option>
             </select>
             <label for="content">Content:</label>
-            <input type="text" id="content" placeholder="Enter content" />
+            <input
+              type="text"
+              id="content"
+              placeholder="Enter content"
+              onChange={handleContentChange}
+            />
           </div>
 
           {/* <div></div> */}
           <div>
-            <button className={"add"} type="submit">
+            <button className={"add"} type="submit" onClick={addContent}>
               Feed Me
             </button>
           </div>
