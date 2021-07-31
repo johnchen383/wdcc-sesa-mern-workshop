a45d22722cb51346e52e16ab5ebe35fcf555a066
John Chen <john.bm.chen@gmail.com>
2021-07-31 12:44:22 +1200

    added redirect button

--- chrome_ext/background.js
+++ chrome_ext/background.js
@@ -1,4 +1,15 @@
-window.location.replace("http://localhost:5500/")
+document.body.innerHTML += '<dialog>Are you sure you want to do work? Your cat is hungry...<br>\
+You should go to Oodle to feed it!<br><button class="b1">You\'re right</button><button class = "b2">I agree</button></dialog>';
+var dialog = document.querySelector("dialog")
+dialog.querySelector(".b1").addEventListener("click", function() {
+    window.location.replace("http://localhost:5500/")
+})
+dialog.querySelector(".b2").addEventListener("click", function() {
+    window.location.replace("http://localhost:5500/")
+})
+dialog.showModal()
+
+
 
 // var host = "http://www.google.com";
 // chrome.webRequest.onBeforeRequest.addListener(
