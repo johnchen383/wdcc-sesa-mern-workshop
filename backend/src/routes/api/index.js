7cae4b37da3fb2b2c34bbf6ff6040fd27bdc728b
Serena Lau (slau981) <serena.lau272@gmail.com>
2021-07-31 13:05:29 +1200

    added some test routes

--- backend/src/routes/api/index.js
+++ backend/src/routes/api/index.js
@@ -3,7 +3,13 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', function(req, res) {
-    res.send("Hello World!");
+    res.send("Hello John!");
 });
+
+router.get('/status', function(req, res) {
+    res.send("Hello yozazazaz!");
+});
+
+router.put('/status', () => {})
 
 module.exports = router;
